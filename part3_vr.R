ff49a6b56b8c81653a48eaff5534aa028db3e72a
vrathbone <vanessa.rathbone@gmail.com>
2019-10-21 13:57:03 -0700

    add infant mortality rate

--- part3_vr.R
+++ part3_vr.R
@@ -11,3 +11,15 @@ library(here)
 db <- read_csv(here::here("data", "disease_burden.csv")) %>% 
   clean_names() %>% 
   rename(deaths_per_100k = death_rate_per_100_000)
+
+# Create subset and graph
+
+db_sub <- db %>% 
+  filter(country_name %in% c("Afghanistan", "United States", "Japan", "Somalia")) %>% 
+  filter(age_group == "0-6 days", sex == "Both")
+
+# Create a basic line graph
+
+ggplot(data = db_sub, aes(x = year, 
+                          y = deaths_per_100k)) +
+  geom_line(aes(color = country_name))
